b72779fc4ef26b6705f1048b0adef8b119c6cbe0
philliphnguyen <phillip.nguyen1997@yahoo.com>
2022-03-26 10:47:56 -0700

    Register/sign in modal for frontend

--- frontend/src/components/MyNavbar.js
+++ frontend/src/components/MyNavbar.js
@@ -4,28 +4,35 @@ import { AiOutlineSearch } from 'react-icons/ai'
 import { LinkContainer } from 'react-router-bootstrap'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
+import SignInModal from './SignInModal'
+import { useState } from 'react'
 
 function MyNavbar() {
-    return (
-        <Navbar className='px-4' bg='dark' variant='dark'>
-            <LinkContainer to='/'>
-                <Navbar.Brand >
-                    Hotels
-                </Navbar.Brand>
-            </LinkContainer>
+    const [show, setShow] = useState(false)
 
-            <div style={{ width: '75%', position: 'relative' }}>
-                <FormControl type='search' className='nav-search'
-                    placeholder='Search for hotels' title='search' />
-                <Button className='rounded-circle search-button ms-2'
-                    size='sm' >
-                    <AiOutlineSearch size={25} />
-                </Button>
-            </div>
-            <Nav className='ms-auto' >
-                <Nav.Link >Sign in</Nav.Link>
-            </Nav>
-        </Navbar>
+    return (
+        <div>
+            <Navbar className='px-4' bg='dark' variant='dark'>
+                <LinkContainer to='/'>
+                    <Navbar.Brand >
+                        Hotels
+                    </Navbar.Brand>
+                </LinkContainer>
+
+                <div style={{ width: '75%', position: 'relative' }}>
+                    <FormControl type='search' className='nav-search'
+                        placeholder='Search for hotels' title='search' />
+                    <Button className='rounded-circle search-button ms-2'
+                        size='sm' >
+                        <AiOutlineSearch size={25} />
+                    </Button>
+                </div>
+                <Nav className='ms-auto' >
+                    <Nav.Link onClick={() => setShow(true)}>Sign in</Nav.Link>
+                </Nav>
+            </Navbar>
+            <SignInModal show={show} handleClose={() => setShow(false)} />
+        </div>
     )
 }
 
