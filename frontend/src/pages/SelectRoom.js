ed62ef5c7b1246bb307d3113ec865383c0c619b7
philliphnguyen <phillip.nguyen1997@yahoo.com>
2022-04-12 13:12:53 -0700

    db and routing setup in backend

--- frontend/src/pages/SelectRoom.js
+++ frontend/src/pages/SelectRoom.js
@@ -4,38 +4,40 @@ import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
+import { useNavigate } from 'react-router-dom'
 
 function SelectRoom() {
+    const navigate = useNavigate()
     return (
         <Container className='text-center'>
             <Row>
                 <Col>
                     <Card style={{ width: '18rem' }} className='mt-3'>
-                        <Card.Img variant='top' src="/imgs/home2.jpg" />
+                        <Card.Img variant='top' src="/imgs/home1.jpg" />
                         <Card.Body>
                             <Card.Title>Single room</Card.Title>
                             <Card.Text>Supporting text</Card.Text>
-                            <Button variant='dark' className='book-room-button'>Book room</Button>
+                            <Button onClick={() => navigate('/bookroom')} variant='dark' className='book-room-button'>Book room</Button>
                         </Card.Body>
                     </Card>
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }} className='mt-3'>
-                        <Card.Img variant='top' src="/imgs/home2.jpg" />
+                        <Card.Img variant='top' src="/imgs/home1.jpg" />
                         <Card.Body>
                             <Card.Title>Double room</Card.Title>
                             <Card.Text>Supporting text</Card.Text>
-                            <Button variant='dark' className='book-room-button'>Book room</Button>
+                            <Button onClick={() => navigate('/bookroom')} variant='dark' className='book-room-button'>Book room</Button>
                         </Card.Body>
                     </Card>
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }} className='mt-3'>
-                        <Card.Img variant='top' src="/imgs/home2.jpg" />
+                        <Card.Img variant='top' src="/imgs/home1.jpg" />
                         <Card.Body>
                             <Card.Title>Suite</Card.Title>
                             <Card.Text>Supporting text</Card.Text>
-                            <Button variant='dark' className='book-room-button'>Book room</Button>
+                            <Button onClick={() => navigate('/bookroom')} variant='dark' className='book-room-button'>Book room</Button>
                         </Card.Body>
                     </Card>
                 </Col>
